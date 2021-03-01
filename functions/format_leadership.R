891e4f058a4572e860dd3d1ff23b5055436baabd
emma-wilson <ewilson8@ed.ac.uk>
2021-03-01 10:38:48 +0000

    simplify leadership role graph

--- functions/format_leadership.R
+++ functions/format_leadership.R
@@ -1,25 +1,26 @@
 format_leadership <- function(df, MajorLeadershipRole){
   
-  # Correct formatting
-  df$MajorLeadershipRole <- sub("I do not have a major leadership role,Other", "I do not have a major leadership role", df$MajorLeadershipRole)
-  df$MajorLeadershipRole <- sub("I run a lab or group,I direct the organisation", "Run lab/group and direct organisation", df$MajorLeadershipRole)
-  df$MajorLeadershipRole <- sub("I run a lab or group,Other", "I run a lab or group", df$MajorLeadershipRole)
-  df$MajorLeadershipRole <- sub("I do not have a major leadership role", "No major leadership role", df$MajorLeadershipRole)
-  df$MajorLeadershipRole <- sub("I run a lab or group", "Run lab/group", df$MajorLeadershipRole)
-  df$MajorLeadershipRole <- sub("I direct the School", "Direct school/organisation", df$MajorLeadershipRole)
-  df$MajorLeadershipRole <- sub("I direct the organisation", "Direct school/organisation", df$MajorLeadershipRole)
+  # Mutate new column
+  df <- mutate(df, Leadership = MajorLeadershipRole)
   
+  # Group
+  df$Leadership <- sub("I do not have a major leadership role,Other", "No", df$Leadership)
+  df$Leadership <- sub("I do not have a major leadership role", "No", df$Leadership)
+  df$Leadership <- sub("I run a lab or group,I direct the organisation", "Yes", df$Leadership)
+  df$Leadership <- sub("I run a lab or group,Other", "Yes", df$Leadership)
+  df$Leadership <- sub("I run a lab or group", "Yes", df$Leadership)
+  df$Leadership <- sub("I direct the School", "Yes", df$Leadership)
+  df$Leadership <- sub("I direct the organisation", "Yes", df$Leadership)
+  df$Leadership <- sub("Other", "Yes", df$Leadership)
+
   # Replace NA
-  df$MajorLeadershipRole[is.na(df$MajorLeadershipRole)] <- "Not reported"
-  
+  df$Leadership[is.na(df$Leadership)] <- "Not reported"
+
   # Change factor levels
-  df$MajorLeadershipRole <- factor(df$MajorLeadershipRole, 
-                                    levels = c("No major leadership role",
-                                               "Run lab/group",
-                                               "Direct school/organisation",
-                                               "Run lab/group and direct organisation",
-                                               "Other",
-                                               "Not reported"))
+  df$Leadership <- factor(df$Leadership,
+                          levels = c("Yes",
+                                     "No",
+                                     "Not reported"))
   
   return(df)
   
