345164dce4b3898aaf24862ff42f8f2b03427b9b
emma-wilson <ewilson8@ed.ac.uk>
2021-03-11 15:52:54 +0000

    separate training wanted and training provided data

--- functions/format_training.R
+++ functions/format_training.R
@@ -1,4 +1,4 @@
-format_training <- function(df, training){
+format_training_familiarity <- function(df, training){
   
   if(training == "Yes"){
     
@@ -14,7 +14,6 @@ format_training <- function(df, training){
     df$Response <- replace(df$Response, grep("I am familiar with this concept", df$Response), "Familiar")
     df$Response <- replace(df$Response, grep("I am NOT familiar with this concept", df$Response), "Not familiar")
     df$Response <- replace(df$Response, grep("I would find training on this useful", df$Response), "Unknown familiarity")
-    df$Response <- replace(df$Response, grep("I think my organisation already provides sufficient training on this", df$Response), "Unknown familiarity")
     
     return(df)
     
@@ -25,19 +24,30 @@ format_training <- function(df, training){
       select(OpenAccess, OpenCode, OpenData, Preprints, Preregistration, Primary, Unit, randomID) %>%
       pivot_longer(cols = OpenAccess:Preregistration, names_to = "OR_Area", values_to = "Response") %>%
       filter(!grepl("I would find training on this useful", Response)) %>%
-      filter(!is.na(Response)) %>%
       mutate(Training = "No")
     
     # Format text responses by organisation provides and/or familiarity
-    df$Response <- replace(df$Response, grep("I think my organisation already provides sufficient training on this", df$Response), "Training provided")
     df$Response <- replace(df$Response, grep("I already engage with this practice", df$Response), "Already engage")
     df$Response <- replace(df$Response, grep("I am familiar with this concept", df$Response), "Familiar")
     df$Response <- replace(df$Response, grep("I am NOT familiar with this concept", df$Response), "Not familiar")
+    df$Response <- replace(df$Response, grep("I think my organisation already provides sufficient training on this", df$Response), "Unknown familiarity")
     
     return(df)
     
   } else{
     print("Error: please select either training = \"Yes\" or \"No\"")
   }
+  
+}
+
+format_training_provided <- function(df){
+  df = df %>%
+    select(OpenAccess, OpenCode, OpenData, Preprints, Preregistration, Primary, Unit, randomID) %>%
+    pivot_longer(cols = OpenAccess:Preregistration, names_to = "OR_Area", values_to = "TrainingProvided") %>%
+    filter(grepl("I think my organisation already provides sufficient training on this", TrainingProvided))
+  
+  df$TrainingProvided <- replace(df$TrainingProvided, grep("I think my organisation already provides sufficient training on this", df$TrainingProvided), "Yes")
+  
+    return(df)
   
 }
