7a42f3471b81c07fcbb38a18a8bca81b74dc4379
emma-wilson <ewilson8@ed.ac.uk>
2021-03-12 10:50:04 +0000

    opendata --> data sharing

--- functions/format_priority.R
+++ functions/format_priority.R
@@ -2,14 +2,14 @@ format_priority <- function(df){
   
   # Subset responses to priority questions
   df = df %>%
-    select(Priority_OpenAccess, Priority_OpenCode, Priority_OpenData, Priority_Preprints, Priority_Preregistration, randomID) %>%
+    select(Priority_OpenAccess, Priority_OpenCode, Priority_DataSharing, Priority_Preprints, Priority_Preregistration, randomID) %>%
     mutate_if(is.integer, as.character) %>%
     filter(!is.na(Priority_OpenAccess))
   
   # Group those ranked 6-8
   df$Priority_OpenAccess <- sub("6|7|8", "6-8", df$Priority_OpenAccess)
   df$Priority_OpenCode <- sub("6|7|8", "6-8", df$Priority_OpenCode)
-  df$Priority_OpenData <- sub("6|7|8", "6-8", df$Priority_OpenData)
+  df$Priority_DataSharing <- sub("6|7|8", "6-8", df$Priority_DataSharing)
   df$Priority_Preprints <- sub("6|7|8", "6-8", df$Priority_Preprints)
   df$Priority_Preregistration <- sub("6|7|8", "6-8", df$Priority_Preregistration)
   
